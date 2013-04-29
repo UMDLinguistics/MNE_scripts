0f2ea0c19d7dd640fa0e7695d4866e3a4f4b38e3
lawchen <lawrencechen@cephalopod.local>
2013-04-29 14:45:55 -0400

    multiple updates to allow script (i) plot multiple conditions on a single graph via for loop, (ii) read a dictionary python script to use condition names, (iii) read a text file to use channels for averaging, (iv) account for python indexing at 0 while mne's indices begin at 1, and (v) set zero baseline for plotting

--- sensor_avg.py
+++ sensor_avg.py
@@ -3,10 +3,14 @@ import mne
 import argparse
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import readInput
+import sys
+sys.path.append("/Users/Shared/Experiments/AUDI/")
+import AUDI_cond as expCond
 
-
+#to run script, type: ipython [this python file name] [experiment name ('exp')] [subject ID ('subjID')] [paradigm ('par')] [name of file containing channels ('chan_filename')] [condition(s) ('condList'); separate multiple conditions with space]
 #example
-#run sensor_avg.py NARWHAL R1704 Narwhal_semprime 7
+#ipython sensor_avg.py AUDI R1524 AUDI_blocked AUDI_R1524_blocked_Bark9_topSourceM100.txt Bark8 Bark9 Bark10 Bark11 
 
 #The last number is the position of the condition in the average file
 
@@ -15,31 +19,49 @@ parser = argparse.ArgumentParser(description='Get input')
 parser.add_argument('exp',type=str)
 parser.add_argument('subjID',type=str)
 parser.add_argument('par',type=str)
-parser.add_argument('cond',type=int) # Bark9, i have input 7
-parser.add_argument('chans',type=int,nargs='+')
+parser.add_argument('chan_filename',type=str, help='include file extension')
+parser.add_argument('condList',type=str,nargs='+',help='condition name will be converted to an integer')
+#parser.add_argument('chans',type=int,nargs='+',help='enter channel number separated by space in between)
 
 args=parser.parse_args()
 
-print args.chans
-##args.chan = args.chan -1  ##this accounts for python indexing at 0
-
 
 ##Filenames
-data_path = '/Users/Shared/Experiments/'+args.exp+'/data/'+args.subjID + '/' #Lawrence added 'Experiments' 4.23.13
+data_path = '/Users/Shared/Experiments/'+args.exp+'/data/'+args.subjID + '/'
 data_file = data_path + args.subjID + '_' + args.par 
 
 print data_path
 print data_file
 
-evoked = mne.fiff.Evoked(data_file +'-ave.fif',setno=args.cond)
 
-meg_data = evoked.data
+chans = readInput.readList(data_path+args.chan_filename)
+chans = [int(chan) for chan in chans]
+print "actual chans as shown in MNE: {}".format(chans)
 
-print meg_data
-print meg_data[args.chans]
+chans = [chan-1 for chan in chans] # this accounts for python indexing at 0
+print "chans as interpreted by iPython: {}".format(chans)
 
-data_to_plot = meg_data[args.chans] #if i say 83, i mean 84
-data_to_plot = np.mean(meg_data[args.chans],0)
+for cond in args.condList:
+    
+	condName = cond
+	print cond
+	cond = expCond.condDict[cond]
+	print "{} is converted {} for python indexing, which is the new cond".format(condName, cond)
+	
+	evoked = mne.fiff.Evoked(data_file +'-ave.fif',setno=cond, baseline=(None,0))
+	
+	meg_data = evoked.data
+	times = evoked.times*1000
+	
+	print meg_data
+	print chans
+	print meg_data[chans]
+	
+	data_to_plot = meg_data[chans]
+	data_to_plot = np.mean(meg_data[chans],0)
+	
+	plt.plot(times,data_to_plot)
 
-plt.plot(data_to_plot)
+
+
 plt.show()
