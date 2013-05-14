84345b893aa7c765caaae584a43cf3c7db5f728c
System Administrator <root@cephalopod.local>
2013-05-14 15:24:54 -0400

    added pickle module for importing experiment dictionary, and added comment headings to organize the script

--- sensor_avg.py
+++ sensor_avg.py
@@ -4,11 +4,9 @@ import argparse
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import readInput
-import sys
-sys.path.append("/Users/Shared/Experiments/AUDI/")
-import AUDI_cond as expCond
+import pprint, pickle
 
-#to run script, type: ipython [this python file name] [experiment name ('exp')] [subject ID ('subjID')] [paradigm ('par')] [name of file containing channels ('chan_filename')] [condition(s) ('condList'); separate multiple conditions with space]
+#to run script, type: ipython [this python file name] [experiment name ('exp')] [subject ID ('subjID')] [paradigm ('par')] [name of file containing the experiment's dictionary('expCondDict')] [name of file containing channels of interest ('chan_filename')] [condition(s) ('condList'); separate multiple conditions with space]
 #example
 #ipython sensor_avg.py AUDI R1524 AUDI_blocked AUDI_R1524_blocked_Bark9_topSourceM100.txt Bark8 Bark9 Bark10 Bark11 
 
@@ -19,19 +17,27 @@ parser = argparse.ArgumentParser(description='Get input')
 parser.add_argument('exp',type=str)
 parser.add_argument('subjID',type=str)
 parser.add_argument('par',type=str)
-parser.add_argument('chan_filename',type=str,help='include file extension')
+parser.add_argument('expCondDict',type=str, help='file contains the experiment condition dictionary; include file extension')
+parser.add_argument('chan_filename',type=str,help='file contains the channels of interest; include file extension')
 parser.add_argument('condList',type=str,nargs='+',help='condition name will be converted to an integer')
 #parser.add_argument('chans',type=int,nargs='+',help='enter channel number separated by space in between)
 parser.add_argument('--rms',action='store_true',help='optional argument: it does root-mean-square of data')
 parser.add_argument('-b','--baseline',help='optional argument: name the baseline condition to give it a thicker plot line')
-#things to add: (1) pickle module so we don't have to import AUDI_cond for everyone
 
 args=parser.parse_args()
 
 
 if args.baseline:
 	if args.baseline not in args.condList:
-		sys.exit("Error: Baseline condition does not exist")
+		sys.exit("Error: Baseline condition does not exist. It must be from condList")
+
+
+##Condition dictionary
+
+dict_file = open('/Users/Shared/Experiments/'+args.exp+'/'+args.expCondDict, 'rb')
+condDict = pickle.load(dict_file)
+pprint.pprint(condDict)
+dict_file.close()
 
 ##Filenames
 data_path = '/Users/Shared/Experiments/'+args.exp+'/data/'+args.subjID + '/'
@@ -40,7 +46,6 @@ data_file = data_path + args.subjID + '_' + args.par
 print data_path
 print data_file
 
-
 chans = readInput.readList(data_path+args.chan_filename)
 chans = [int(chan) for chan in chans]
 print "actual chans as shown in MNE: {}".format(chans)
@@ -48,14 +53,16 @@ print "actual chans as shown in MNE: {}".format(chans)
 chans = [chan-1 for chan in chans] # this accounts for python indexing at 0
 print "chans as interpreted by iPython: {}".format(chans)
 
+
+##Plotting condition(s)
 for cond in args.condList:
 	
 	condName = cond
 	print "Processing {}...".format(condName)
-	cond = expCond.condDict[cond]
-	print "{} is converted {} for python indexing, which is the new cond".format(condName, cond)
+	condCode = condDict[cond]
+	print "{} is converted {} for python indexing".format(condName, condCode)
 	
-	evoked = mne.fiff.Evoked(data_file +'-ave.fif',setno=cond, baseline=(None,0))
+	evoked = mne.fiff.Evoked(data_file +'-ave.fif',setno=condCode, baseline=(None,0))
 	
 	meg_data = evoked.data
 	times = evoked.times*1000
@@ -65,7 +72,8 @@ for cond in args.condList:
 	print meg_data[chans]
 	
 	data_to_plot = meg_data[chans]
-	
+
+	# optional rms:
 	if args.rms:
 		data_to_plot = np.sqrt(np.mean(meg_data[chans]**2,0))
 		print "root-mean-square applied"
@@ -73,8 +81,7 @@ for cond in args.condList:
 		data_to_plot = np.mean(meg_data[chans],0)
 		print "no root-mean-square applied"
 	
-#	plt.plot(times,data_to_plot)
-
+    # optional baseline:
 	
 	if args.baseline==condName: # Baseline condition gets extra thick line
 		plt.plot(times,data_to_plot,linewidth=3)
