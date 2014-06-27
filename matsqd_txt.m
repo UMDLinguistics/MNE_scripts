11fe4e3a150c010c26200891f9b78d49f6ca4214
Ellen Lau <ellenlau@umd.edu>
2014-06-27 09:18:52 -0400

    fixed typo and comments in matsqd_txt.m

--- matsqd_txt.m
+++ matsqd_txt.m
@@ -3,7 +3,7 @@ function matsqd_txt(expName, subjID, paradigmName)
 %Data needs to be stored in
 %/Users/Shared/Experiments/expName/data/subjID/KIT
 %Example 
-%matsqd_txt('AUDI','R0253','AUDI_blocked-Filtered')
+%matsqd_txt('NARWHAL', 'R2118','Narwhal_semprime_06.10.14-Filtered');
 
 inPath = strcat('/Volumes/CUTTLEFISH/MEG_Experiments/',expName,'/data/',subjID,'/KIT/');
 inFile = strcat(inPath,subjID,'_',paradigmName)
@@ -12,4 +12,4 @@ data = sqdread(strcat(inFile,'.sqd'));
 data(:,1:157) = data(:,1:157)*1e-15;  %%return to Tesla, but don't rescale trigger lines
 
 outFile = strcat(inFile,'-matexp.txt')
-dlmwrite(outFile,data_scale,'\t');
+dlmwrite(outFile,data,'\t');
